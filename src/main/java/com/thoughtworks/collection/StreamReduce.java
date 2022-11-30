34795674f93131a4c9fced7be802446bc0e3ccf1
kwokth4 <kwokth4@oocl.com>
2022-11-30 14:26:47 +0800

    Finish

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -5,14 +5,23 @@ import java.util.List;
 public class StreamReduce {
 
     public int getLastOdd(List<Integer> numbers) {
-        return 0;
+        return numbers
+                .stream()
+                .reduce((number1,number2) -> number2%2!=0?number2:number1)
+                .get();
     }
 
     public String getLongest(List<String> words) {
-        return null;
+        return words
+                .stream()
+                .reduce((word1,word2) -> word1.length()<word2.length()?word2:word1)
+                .get();
     }
 
     public int getTotalLength(List<String> words) {
-        return 0;
+        return words
+                .stream()
+                .mapToInt(word -> word.length())
+                .reduce(0, (word1, word2) -> word1 + word2);
     }
 }
